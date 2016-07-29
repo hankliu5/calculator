c4a7097b086f46755134e31b5c8803b33eff0287
hankliu5 <liu19901124@gmail.com>
2016-07-29 15:30:58 -0700

    finished lecture 3

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -11,9 +11,11 @@ import Foundation
 class CalculatorBrain {
   
   private var accumulator = 0.0
+  private var internalProgram = [AnyObject]()
   
   func setOperand(operand: Double) {
     accumulator = operand
+    internalProgram.append(operand)
   }
   
   var operations: Dictionary<String, Operation> = [
@@ -37,6 +39,7 @@ class CalculatorBrain {
   }
   
   func performOperation(symbol: String) {
+    internalProgram.append(symbol)
     if let operation = operations[symbol] {
       switch operation {
       case .Constant(let value):
@@ -58,6 +61,34 @@ class CalculatorBrain {
       pending = nil
     }
   }
+  
+  typealias PropertyList = AnyObject
+  
+  var program: PropertyList {
+    get {
+      return internalProgram
+    }
+    
+    set {
+      clear()
+      if let arrayOfOps = newValue as? [AnyObject] {
+        for op in arrayOfOps {
+          if let operand = op as? Double {
+            setOperand(operand)
+          } else if let operation = op as? String {
+            performOperation(operation)
+          }
+        }
+      }
+    }
+  }
+  
+  func clear() {
+    accumulator = 0.0
+    pending = nil
+    internalProgram.removeAll()
+  }
+  
   private var pending: PendingBinaryOpeationInfo?
   
   private struct PendingBinaryOpeationInfo {
